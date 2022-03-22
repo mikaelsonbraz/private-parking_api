42c51f4c2ad7f1b9bb9e55f06f4ee908ed969a2e
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-03-22 16:48:45 -0300

    Agora sim

--- src/main/java/com/mikaelsonbraz/parkingapi/api/rent/model/repository/RentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mikaelsonbraz.parkingapi.api.rent.model.repository;
-
-import com.mikaelsonbraz.parkingapi.api.rent.model.entity.Rent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentRepository extends JpaRepository<Rent, Integer> {
-}
