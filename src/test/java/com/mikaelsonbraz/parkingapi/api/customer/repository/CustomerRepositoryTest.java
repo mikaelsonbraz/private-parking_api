d25f46c7fc65da24e2118023da91c15d70afe806
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-04-12 13:49:27 -0300

    Adding MySQL as project database

--- src/test/java/com/mikaelsonbraz/parkingapi/api/customer/repository/CustomerRepositoryTest.java
+++ src/test/java/com/mikaelsonbraz/parkingapi/api/customer/repository/CustomerRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
@@ -17,6 +18,7 @@ import java.util.Optional;
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CustomerRepositoryTest {
 
     @Autowired
--- src/test/java/com/mikaelsonbraz/parkingapi/api/renting/repository/RentingRepositoryTest.java
+++ src/test/java/com/mikaelsonbraz/parkingapi/api/renting/repository/RentingRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RentingRepositoryTest {
 
     @Autowired
